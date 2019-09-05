b46cf07181025a22f827a82db2e9fbcb645a7bf8
Ruben Funai <ruben.funai@gmail.com>
2019-09-05 22:24:51 +1000

    Added bitbucket configuration

---
+++ config/bitbucket.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type {import('@adonisjs/framework/src/Env')} */
+const Env = use('Env')
+
+module.exports = {
+  baseUrl: Env.get('BITBUCKET_BASE_URL', 'https://api.bitbucket.org/2.0'),
+  timeout: Env.get('BITBUCKET_TIMEOUT', 60),
+  auth: {
+    type: Env.get('BITBUCKET_AUTH_TYPE', 'apppassword'),
+    token: {
+      type: 'token',
+      token: Env.get('BITBUCKET_TOKEN', ''),
+    },
+    apppassword: {
+      type: 'apppassword',
+      username: Env.get('BITBUCKET_USERNAME', ''),
+      password: Env.get('BITBUCKET_PASSWORD', '')
+    },
+    basic: {
+      type: 'basic',
+      username: Env.get('BITBUCKET_USERNAME', ''),
+      password: Env.get('BITBUCKET_PASSWORD', '')
+    }
+  }
+}
